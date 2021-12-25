f21b7d0e6aef3e6f02fe86a47731841597837686
i-xi-dev <naoyuki.isono@gmail.com>
2021-12-26 01:15:49 +0900

    0.0.1

--- src/media_type.test.ts
+++ src/media_type.test.ts
@@ -55,3 +55,240 @@ describe("MediaType.prototype.essence", () => {
   });
 
 });
+
+describe("MediaType.fromString", () => {
+  it("fromString(string)", () => {
+    expect(MediaType.fromString("text/plain").toString()).toBe("text/plain");
+    expect(MediaType.fromString(" text/plain ").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain;").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ;").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; ").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset ").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset=utf-8 ").toString()).toBe("text/plain;charset=utf-8");
+    expect(MediaType.fromString("text/plain ;charset=UTF-8").toString()).toBe("text/plain;charset=UTF-8");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test").toString()).toBe("text/plain;charset=utf-8");
+    expect(MediaType.fromString("text/plain ;charset=utf-8; test=test2").toString()).toBe("text/plain;charset=utf-8;test=test2");
+    expect(MediaType.fromString("text/plain ;charset=utf-8 ; test=test2").toString()).toBe("text/plain;charset=utf-8;test=test2");
+    expect(MediaType.fromString("text/plain ;charset =utf-8 ; test=test2").toString()).toBe("text/plain;test=test2");
+    expect(MediaType.fromString("text/plain ;charset= utf-8 ; test=test2").toString()).toBe("text/plain;charset=\" utf-8\";test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"utf-8\" ; test=test2").toString()).toBe("text/plain;charset=utf-8;test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\f-8\" ; test=t\\est,2").toString()).toBe("text/plain;charset=utf-8;test=\"t\\\\est,2\"");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\\"f-8\" ; test=test2").toString()).toBe("text/plain;charset=\"ut\\\"f-8\";test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\\ ; test=test2").toString()).toBe("text/plain;charset=\"\\\\\";test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"\\ ; test=test2").toString()).toBe("text/plain;charset=\" ; test=test2\"");
+    expect(MediaType.fromString("text/plain ;charset=\" ; test=test2").toString()).toBe("text/plain;charset=\" ; test=test2\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\" ; test=test2").toString()).toBe("text/plain;charset=;test=test2");
+    expect(MediaType.fromString('text/plain ;charset="utf-16" utf-8 ; test=test2').toString()).toBe("text/plain;charset=utf-16;test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"\\").toString()).toBe("text/plain;charset=\"\\\\\"");
+    expect(MediaType.fromString('text/plain ;charset="aa\\\\a\\"a"').toString()).toBe("text/plain;charset=\"aa\\\\a\\\"a\"");
+    expect(MediaType.fromString('text/plain ;charset=a;x="http://example.com/x?a=1"').toString()).toBe("text/plain;charset=a;x=\"http://example.com/x?a=1\"");
+    expect(MediaType.fromString('text/plain ;x="http://example.com/x?a=1";charset=a').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=a");
+    expect(MediaType.fromString('text/plain ; x="http://example.com/x?a=1" ;charset=a').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=a");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test=test2;charset=shift_jis").toString()).toBe("text/plain;charset=utf-8;test=test2");
+
+    expect(() => {
+      MediaType.fromString("text/plain,");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+    expect(() => {
+      MediaType.fromString(" text/plain ,");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+    expect(MediaType.fromString("text/plain;,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ;,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; ,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset ,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset=utf-8 ,").toString()).toBe("text/plain;charset=\"utf-8 ,\"");
+    expect(MediaType.fromString("text/plain ;charset=UTF-8,").toString()).toBe("text/plain;charset=\"UTF-8,\"");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test,").toString()).toBe("text/plain;charset=utf-8");
+    expect(MediaType.fromString("text/plain ;charset=utf-8; test=test2,").toString()).toBe("text/plain;charset=utf-8;test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=utf-8 ; test=test2,").toString()).toBe("text/plain;charset=utf-8;test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset =utf-8 ; test=test2,").toString()).toBe("text/plain;test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset= utf-8 ; test=test2,").toString()).toBe("text/plain;charset=\" utf-8\";test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"utf-8\" ; test=test2,").toString()).toBe("text/plain;charset=utf-8;test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\f-8\" ; test=t\\est,2,").toString()).toBe("text/plain;charset=utf-8;test=\"t\\\\est,2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\\"f-8\" ; test=test2,").toString()).toBe("text/plain;charset=\"ut\\\"f-8\";test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\\ ; test=test2,").toString()).toBe("text/plain;charset=\"\\\\\";test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\\ ; test=test2,").toString()).toBe("text/plain;charset=\" ; test=test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\" ; test=test2,").toString()).toBe("text/plain;charset=\" ; test=test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\" ; test=test2,").toString()).toBe("text/plain;charset=;test=\"test2,\"");
+    expect(MediaType.fromString('text/plain ;charset="utf-16" utf-8 ; test=test2,').toString()).toBe("text/plain;charset=utf-16;test=\"test2,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\\,").toString()).toBe("text/plain;charset=\",\"");
+    expect(MediaType.fromString('text/plain ;charset="aa\\\\a\\"a",').toString()).toBe("text/plain;charset=\"aa\\\\a\\\"a\"");
+    expect(MediaType.fromString('text/plain ;charset=a;x="http://example.com/x?a=1",').toString()).toBe("text/plain;charset=a;x=\"http://example.com/x?a=1\"");
+    expect(MediaType.fromString('text/plain ;x="http://example.com/x?a=1";charset=a,').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=\"a,\"");
+    expect(MediaType.fromString('text/plain ; x="http://example.com/x?a=1" ;charset=a,').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=\"a,\"");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test=test2;charset=shift_jis,").toString()).toBe("text/plain;charset=utf-8;test=test2");
+
+    expect(() => {
+      MediaType.fromString("text/plain,%3C");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+    expect(() => {
+      MediaType.fromString(" text/plain ,%3C");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+    expect(MediaType.fromString("text/plain;,%3C").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ;,%3C").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; ,%3C").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset,%3C").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset ,%3C").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset=utf-8 ,%3C").toString()).toBe("text/plain;charset=\"utf-8 ,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=UTF-8,%3C").toString()).toBe("text/plain;charset=\"UTF-8,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test,%3C").toString()).toBe("text/plain;charset=utf-8");
+    expect(MediaType.fromString("text/plain ;charset=utf-8; test=test2,%3C").toString()).toBe("text/plain;charset=utf-8;test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=utf-8 ; test=test2,%3C").toString()).toBe("text/plain;charset=utf-8;test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset =utf-8 ; test=test2,%3C").toString()).toBe("text/plain;test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset= utf-8 ; test=test2,%3C").toString()).toBe("text/plain;charset=\" utf-8\";test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\"utf-8\" ; test=test2,%3C").toString()).toBe("text/plain;charset=utf-8;test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\f-8\" ; test=t\\est,2,%3C").toString()).toBe("text/plain;charset=utf-8;test=\"t\\\\est,2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\\"f-8\" ; test=test2,%3C").toString()).toBe("text/plain;charset=\"ut\\\"f-8\";test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\\ ; test=test2,%3C").toString()).toBe("text/plain;charset=\"\\\\\";test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\\ ; test=test2,%3C").toString()).toBe("text/plain;charset=\" ; test=test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\" ; test=test2,%3C").toString()).toBe("text/plain;charset=\" ; test=test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\" ; test=test2,%3C").toString()).toBe("text/plain;charset=;test=\"test2,%3C\"");
+    expect(MediaType.fromString('text/plain ;charset="utf-16" utf-8 ; test=test2,%3C').toString()).toBe("text/plain;charset=utf-16;test=\"test2,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\\,%3C").toString()).toBe("text/plain;charset=\",%3C\"");
+    expect(MediaType.fromString('text/plain ;charset="aa\\\\a\\"a",%3C').toString()).toBe("text/plain;charset=\"aa\\\\a\\\"a\"");
+    expect(MediaType.fromString('text/plain ;charset=a;x="http://example.com/x?a=1",%3C').toString()).toBe("text/plain;charset=a;x=\"http://example.com/x?a=1\"");
+    expect(MediaType.fromString('text/plain ;x="http://example.com/x?a=1";charset=a,%3C').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=\"a,%3C\"");
+    expect(MediaType.fromString('text/plain ; x="http://example.com/x?a=1" ;charset=a,%3C').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=\"a,%3C\"");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test=test2;charset=shift_jis,%3C").toString()).toBe("text/plain;charset=utf-8;test=test2");
+
+    expect(MediaType.fromString("text/plain;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString(" text/plain ;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain;;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ;;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; ;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset ;base64,").toString()).toBe("text/plain");
+    expect(MediaType.fromString("text/plain ; charset=utf-8 ;base64,").toString()).toBe("text/plain;charset=utf-8");
+    expect(MediaType.fromString("text/plain ;charset=UTF-8;base64,").toString()).toBe("text/plain;charset=UTF-8");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test;base64,").toString()).toBe("text/plain;charset=utf-8");
+    expect(MediaType.fromString("text/plain ;charset=utf-8; test=test2;base64,").toString()).toBe("text/plain;charset=utf-8;test=test2");
+    expect(MediaType.fromString("text/plain ;charset=utf-8 ; test=test2;base64,").toString()).toBe("text/plain;charset=utf-8;test=test2");
+    expect(MediaType.fromString("text/plain ;charset =utf-8 ; test=test2;base64,").toString()).toBe("text/plain;test=test2");
+    expect(MediaType.fromString("text/plain ;charset= utf-8 ; test=test2;base64,").toString()).toBe("text/plain;charset=\" utf-8\";test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"utf-8\" ; test=test2;base64,").toString()).toBe("text/plain;charset=utf-8;test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\f-8\" ; test=t\\est,2;base64,").toString()).toBe("text/plain;charset=utf-8;test=\"t\\\\est,2\"");
+    expect(MediaType.fromString("text/plain ;charset=\"ut\\\"f-8\" ; test=test2;base64,").toString()).toBe("text/plain;charset=\"ut\\\"f-8\";test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\\ ; test=test2;base64,").toString()).toBe("text/plain;charset=\"\\\\\";test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"\\ ; test=test2;base64,").toString()).toBe("text/plain;charset=\" ; test=test2;base64,\"");
+    expect(MediaType.fromString("text/plain ;charset=\" ; test=test2;base64,").toString()).toBe("text/plain;charset=\" ; test=test2;base64,\"");
+    expect(MediaType.fromString("text/plain ;charset=\"\" ; test=test2;base64,").toString()).toBe("text/plain;charset=;test=test2");
+    expect(MediaType.fromString('text/plain ;charset="utf-16" utf-8 ; test=test2;base64,').toString()).toBe("text/plain;charset=utf-16;test=test2");
+    expect(MediaType.fromString("text/plain ;charset=\"\\;base64,").toString()).toBe("text/plain;charset=\";base64,\"");
+    expect(MediaType.fromString('text/plain ;charset="aa\\\\a\\"a";base64,').toString()).toBe("text/plain;charset=\"aa\\\\a\\\"a\"");
+    expect(MediaType.fromString('text/plain ;charset=a;x="http://example.com/x?a=1";base64,').toString()).toBe("text/plain;charset=a;x=\"http://example.com/x?a=1\"");
+    expect(MediaType.fromString('text/plain ;x="http://example.com/x?a=1";charset=a;base64,').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=a");
+    expect(MediaType.fromString('text/plain ; x="http://example.com/x?a=1" ;charset=a;base64,').toString()).toBe("text/plain;x=\"http://example.com/x?a=1\";charset=a");
+    expect(MediaType.fromString("text/plain ;charset=utf-8;test=test2;charset=shift_jis;base64,").toString()).toBe("text/plain;charset=utf-8;test=test2");
+
+    expect(() => {
+      MediaType.fromString("text");
+    }).toThrowError({
+      name: "TypeError",
+      message: "typeName"
+    });
+
+    expect(() => {
+      MediaType.fromString("text/");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+    expect(() => {
+      MediaType.fromString("/test");
+    }).toThrowError({
+      name: "TypeError",
+      message: "typeName"
+    });
+
+    expect(() => {
+      MediaType.fromString("/");
+    }).toThrowError({
+      name: "TypeError",
+      message: "typeName"
+    });
+
+    expect(() => {
+      MediaType.fromString("");
+    }).toThrowError({
+      name: "TypeError",
+      message: "typeName"
+    });
+
+    expect(() => {
+      MediaType.fromString("text/t/t");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+    expect(() => {
+      MediaType.fromString("text/t,t");
+    }).toThrowError({
+      name: "TypeError",
+      message: "subtypeName"
+    });
+
+  });
+
+});
+
+describe("MediaType.prototype.getParameterValue", () => {
+  it("getParameterValue(string)", () => {
+    const i0 = MediaType.fromString("text/plain");
+    expect(i0.getParameterValue("charset")).toBeNull();
+
+    const i1 = MediaType.fromString("text/plain;charset=uTf-8");
+    expect(i1.getParameterValue("charset")).toBe("uTf-8");
+
+    const i2 = MediaType.fromString("text/plain;CHARSET=uTf-8 ");
+    expect(i2.getParameterValue("charset")).toBe("uTf-8");
+
+    const i3 = MediaType.fromString("text/plain;charset=uTf-8 ; x=9");
+    expect(i3.getParameterValue("charset")).toBe("uTf-8");
+
+    const i4 = MediaType.fromString("text/plain;charset=\"uTf-8\" ; x=9");
+    expect(i4.getParameterValue("charset")).toBe("uTf-8");
+
+    const i5 = MediaType.fromString("text/plain;  charset=\"uTf-8 \"; x=9");
+    expect(i5.getParameterValue("charset")).toBe("uTf-8 ");
+
+  });
+
+});
+
+describe("MediaType.prototype.hasParameter", () => {
+  it("hasParameter(string)", () => {
+    const i0 = MediaType.fromString("text/plain");
+    expect(i0.hasParameter("charset")).toBe(false);
+
+    const i1 = MediaType.fromString("text/plain;charset=uTf-8");
+    expect(i1.hasParameter("charset")).toBe(true);
+
+    const i2 = MediaType.fromString("text/plain;CHARSET=uTf-8 ");
+    expect(i2.hasParameter("charset")).toBe(true);
+
+    const i5 = MediaType.fromString("text/plain;  charset=\"uTf-8 \"; x=9");
+    expect(i5.hasParameter("charset")).toBe(true);
+
+  });
+
+});
