27543e7ce40ee8b218fb9810b93c5e12f1a626ce
i-xi-dev <naoyuki.isono@gmail.com>
2023-08-19 11:56:37 +0900

    1.2.9

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { Http } from "https://raw.githubusercontent.com/i-xi-dev/http.es/1.0.5/mod.ts";
-export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.8/mod.ts";
-export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.2/mod.ts";
+export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.10/mod.ts";
+export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.4/mod.ts";
