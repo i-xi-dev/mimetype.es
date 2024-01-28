c2c2f90f7091f262c1047bad00091c8e7c56d648
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-28 16:12:13 +0900

    1.2.20

--- build_npm.ts
+++ build_npm.ts
@@ -15,7 +15,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/mimetype",
-    version: "1.2.19",
+    version: "1.2.20",
     description:
       "A JavaScript MIME type parser and serializer, implements the MIME type defined in WHATWG MIME Sniffing Standard.",
     license: "MIT",
--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { Http } from "https://raw.githubusercontent.com/i-xi-dev/http.es/1.0.11/mod.ts";
-export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.22/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.2/mod.ts";
+export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.23/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
